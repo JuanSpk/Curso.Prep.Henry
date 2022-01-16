d420b0e8bca146be9a7446db8e87f300d3dba961
Juan Pablo <spk.juanpablo@gmail.com>
2022-01-15 23:11:12 -0300

    Homework-3 Finished

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -172,7 +172,7 @@ function esVerdadero(valor){
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
-  //Escribe tu código aquí 
+  //Escribe tu código aquí.
   let arrayTablaDel6 = []
   for (let i = 0; i < 11; i++) {
         arrayTablaDel6.push(6 * i)
@@ -203,7 +203,6 @@ function doWhile(numero) {
   while(i < 8);
   return a;
 }
-
 
 // No modificar nada debajo de esta línea
 // --------------------------------
